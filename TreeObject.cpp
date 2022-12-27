91370b058cd6a7fa855c5b56e17099a61b7f3b42
BartoLiza <>
2022-12-27 14:09:23 +0300

    Исправление ошибки, если в одной папке 2 файла с искомым названием

--- TreeObject.cpp
+++ TreeObject.cpp
@@ -6,7 +6,7 @@ TreeObject::TreeObject(string fullPath) : TreeObject((fs::path)fullPath)
 {
 }
 
-TreeObject::TreeObject(fs::path fullPath) : TreeObject(fullPath, fullPath.stem().string())
+TreeObject::TreeObject(fs::path fullPath) : TreeObject(fullPath, fullPath.stem().string()) // stem() - возвращает только имя
 {
 }
 
@@ -16,7 +16,7 @@ TreeObject::TreeObject(fs::path fullPath, string name)
 	this->name = name;
 }
 
-FileObject::FileObject(string fullPath) : TreeObject(fullPath) {}
+FileObject::FileObject(string fullPath) : TreeObject(fullPath) {} //вывызается родительский конструктор
 FileObject::FileObject(fs::path fullPath) : TreeObject(fullPath) {}
 
 DirObject::DirObject(string fullPath) : TreeObject(fullPath) {}
@@ -25,36 +25,35 @@ DirObject::DirObject(fs::path fullPath) : TreeObject(fullPath) {}
 bool DirObject::checkDir()
 {
 	try {
-		fs::directory_iterator dirIter = fs::directory_iterator{ fullPath };
-		return true;
+		fs::directory_iterator dirIter = fs::directory_iterator{ fullPath }; // проверяем доступность содержимого папки
+		return true; // доступно!
 	}
 	catch (exception)
 	{
-		return false;
+		return false; // не доступно! (системные файлы, которые мы не можем открывать)
 	}
 }
 
 void DirObject::collectChilds()
 {
-	try {
-		fs::directory_iterator dirIter = fs::directory_iterator{ fullPath };
+	try { // проверка папки на доступность открытия
+		fs::directory_iterator dirIter = fs::directory_iterator{ fullPath }; //  directory_iterator - указатель на пути внутри папки
 
-		for (fs::directory_iterator iter = fs::begin(dirIter), endIter = fs::end(dirIter); iter != endIter; iter++)
+		for (fs::directory_iterator iter = fs::begin(dirIter), endIter = fs::end(dirIter); iter != endIter; iter++) // сбор указателей на содержимое папки
 		{
 			try {
-				fs::directory_entry dir_entry = *iter;
-				if (dir_entry.is_directory())
+				fs::directory_entry dir_entry = *iter; // directory_entry - папка или файл внутри папки, берем текущий элемент
+				if (dir_entry.is_directory()) // является ли текущий элемент папкой?
 				{
-					//DirObject* dir = new DirObject(p);
-					DirObject* dir = new DirObject(dir_entry.path());
-					if (dir->checkDir())
+					DirObject* dir = new DirObject(dir_entry.path()); // создаем новый элемент дерева (класса папки) 
+					if (dir->checkDir()) // если доступна
 					{
-						dirs.push_back(dir);
+						dirs.push_back(dir); // кладем папку в дерево
 					}
 				}
 				else
 				{
-					FileObject* file = new FileObject(dir_entry.path());
+					FileObject* file = new FileObject(dir_entry.path());// создаем новый элемент дерева (класса файла) 
 					files.push_back(file);
 				}
 			}
@@ -70,10 +69,10 @@ void DirObject::collectChilds()
 	}
 }
 
-void DirObject::collectChildsRecursive()
+void DirObject::collectChildsRecursive() // 
 {
 	collectChilds();
-	for (DirObject* dir : dirs)
+	for (DirObject* dir : dirs) // перебираем все элементы ввектора dirs
 	{
 		dir->collectChildsRecursive();
 	}
@@ -81,36 +80,34 @@ void DirObject::collectChildsRecursive()
 
 vector<string>* DirObject::findFile(const string fileName, ThreadPool* threadPool)
 {
-	if (threadPool->tryGetThread())
+	if (threadPool->tryGetThread()) // если есть сободный поток
 	{
-		thread thr(findFileThread, this, std::cref(fileName), threadPool);
-		thr.join();
+		thread thr(findFileThread, this, fileName, threadPool); // создаем новый поток и передаем в функцию потока текущий узел, имя искомого файла и пул потоков
+		thr.join(); // ожидаем окончание выполнения потока
 	}
 
-	vector<string>* res = results.getResults();
+	vector<string>* res = results.getResults(); // 
 	return res;
 }
 
-void findFileThread(DirObject* dir, const string fileName, ThreadPool* threadPool)
+void findFileThread(DirObject* dir, const string fileName, ThreadPool* threadPool) // функция потока поиска
 {
-	for (FileObject* file : dir->files)
+	for (FileObject* file : dir->files) // перебираем файлы в текущей папке
 	{
-		if (file->name == fileName)
-		{	
-			results.putResult(file->fullPath.string());
-
-			break;
+		if (file->name == fileName) // если найдено совпадение имени файла
+		{
+			results.putResult(file->fullPath.string()); //кладем путь найденого файла в результаты
 		}
 	}
 
-	if (dir->dirs.size()>0)
+	if (dir->dirs.size() > 0) // если кол-во папок 
 	{
 		vector<DirObject*> dirsToFindInThread;
 
 		vector<thread> childThreads;
 
 
-		for (int i = 0; i+1 < dir->dirs.size(); i++)
+		for (int i = 0; i + 1 < dir->dirs.size(); i++)
 		{
 			if (threadPool->tryGetThread())
 			{
@@ -137,7 +134,7 @@ void findFileThread(DirObject* dir, const string fileName, ThreadPool* threadPoo
 
 		dirsToFindInThread.clear();
 
-		findFileThread(dir->dirs[dir->dirs.size()-1], fileName, threadPool);
+		findFileThread(dir->dirs[dir->dirs.size() - 1], fileName, threadPool);
 
 		for (int i = 0; i < childThreads.size(); i++)
 		{
